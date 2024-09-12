9415edc432de217130917604b39387b45aa1599d
Alireza Baloochi <Dev.AlirezaBaloochi@gmail.com>
2024-09-13 01:52:15 +0330

    Update default_config.sh

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -75,12 +75,17 @@ function install_docker() {
     ca-certificates \ 
     curl \
     software-properties-common
-    
+
+    echo "=====> download docker gpg ..."
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "=====> running apt-get update"
     apt-get update
+    echo "=====> running apt-cache policy docker-ce"
     apt-cache policy docker-ce
+    echo "=====> running apt-get install -y docker-ce"
     apt-get install -y docker-ce
+    echo "=====> systemctl status docker"
     systemctl status docker
 }
 
